24f2cffdbc992c9a619c6e641f5d703fd06104e8
sachit24 <sachitsapra@gmail.com>
2025-11-21 12:05:48 -0500

    Completed Task1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,38 @@
 package theater;
 
+/**
+ * Represents a theatrical play with a name and genre type.
+ * <p>
+ * This class encapsulates the basic information about a play that might be
+ * performed in a theater, including its title and the type of performance.
+ * </p>
+ *
+ * @author [Your Name]
+ * @version 1.0
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
     public Play(String name, String type) {
+        this.setName(name);
+        this.setType(type);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
         this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
         this.type = type;
     }
 }
